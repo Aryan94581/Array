6c0fd6fceb40a0bcd7ba4dbac276f2b0c7d682f5
Aryan94581 <swamiaryan611@gmail.com>
2024-06-11 15:30:10 +0530

    update

--- A_Scoping.java
+++ A_Scoping.java
@@ -1,3 +1,11 @@
 public class A_Scoping {
-    
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println(num);
+        {
+            System.out.println(num);  //this will not giving error because the value is in outer scope.
+            int num2 = 5;           
+        }
+        System.out.println(num2); //this will raise error because we cannot excess a low scope value in outer scope .
+    }
 }
