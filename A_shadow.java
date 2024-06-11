8b1ef022650edf7bcea844936d22c3b1cfc20ffe
Aryan94581 <swamiaryan611@gmail.com>
2024-06-11 16:18:21 +0530

    update

--- A_shadow.java
+++ A_shadow.java
@@ -1,15 +1,19 @@
 public class A_shadow {
     static int A = 10;
     public static void main(String[] args) {
-        System.out.println(A); // we are able to access it because int A is declared in a higher scope.  
+        System.out.println(A); // we are able to access it 
+        // because int A is declared in a higher scope.  
         // The concept of shadowing is something like this 
         A = 5; // we change the value but the question is  is it changed?
         System.err.println(A); // now what will be the output?
-    //   output is 5 because we shadowed the main A to different value which can be accessed by only in main class
+    //   output is 5 because we shadowed the main A to different value 
+    //    which can be accessed by only in main class
     }
-    {
+    
+    static {
         // wha now ?
         // what is the value of A is?
+        System.out.println(" this is static init");
         System.out.println(A); 
     }
 }
