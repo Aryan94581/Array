3a51a64863ce9e85229e6ad69ba6ad1f1260e421
Aryan94581 <swamiaryan611@gmail.com>
2024-06-12 16:08:43 +0530

    update

---
+++ MultiArrayListCode.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class MultiArrayListCode {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in); 
+    ArrrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+    for(int i = 0; i < 3;i++){
+        list.add(new ArrayList<>());
+    }
+
+
+
+    }
+}
