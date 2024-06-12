0057fef362dba4e981b8bb8e0a12ff3bab4eb5c8
Aryan94581 <swamiaryan611@gmail.com>
2024-06-12 17:13:38 +0530

    update

--- A_reverseArray.java
+++ A_reverseArray.java
@@ -1,3 +1,26 @@
-public class A_reverseArray {
-    
+import java.util.Arrays;
+
+    public class A_reverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(arr));
+
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] Array){
+        int start = 0;
+        int end = Array.length-1;
+        //kab tak chlle loop
+        while(start < end){
+            swap(Array, start, end);
+            start++;
+            end--;
+        }       
+    }
+    static void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1]= array[index2];
+        array[index2] = temp;
+    }
 }
