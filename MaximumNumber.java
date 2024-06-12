c0279424ad3872b00b7fcf0d4225e433e6f99a1c
Aryan94581 <swamiaryan611@gmail.com>
2024-06-12 16:25:07 +0530

    update

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,3 +1,15 @@
 public class MaximumNumber {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 6, 3, 9};
+        System.out.println(MaxNum(arr));
+    }
+    static int MaxNum(int[] Array){
+        int a = Array[0];
+        for (int i : Array) {
+            if( i > a){
+                a = i;
+            }
+        }
+        return a;
+    }
 }
