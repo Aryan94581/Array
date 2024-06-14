11b17ac3b91bfc3815c00f8961506c373108c9b3
Aryan94581 <swamiaryan611@gmail.com>
2024-06-14 16:24:59 +0530

    update

--- A_linearSearch.java
+++ A_linearSearch.java
@@ -1,17 +1,17 @@
 public class A_linearSearch {
 public static void main(String[] args) {
-    System.out.println(linearSearch(Array, 12));
+    int[] Array = {5, 6, 3, 11, 13, 12, 19};
+    System.out.println("Your element is at index of : " + linearSearch(Array, 12));
 }
     static int linearSearch(int[] array, int target){
         if(array.length > 0){
             int i = 0;
-        while(i < array.length-1) {
-            if(target == array[i]){
-                System.out.println("Your element is at index of i");   
+            while(i < array.length-1) {
+                if(target == array[i]){   
+                    return i; 
+                }
+                i++;
             }
-            i++;
-        }
-        return i; 
         }
         return -1;
     }
