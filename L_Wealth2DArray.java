30acd40ecc1fa6fcbb5b6a5a7fcfa5942b7bdde0
Aryan94581 <swamiaryan611@gmail.com>
2024-06-15 15:21:50 +0530

    update

--- L_Wealth2DArray.java
+++ L_Wealth2DArray.java
@@ -1,6 +1,24 @@
+
+import java.util.ArrayList;
+
 //richest-customer-wealth
 public class L_Wealth2DArray {
     public static void main(String[] args) {
-        
+    
+    
+    
+    
     }
+    static ArrayList<Integer> wealthCount(int[][] arr){
+        ArrayList<Integer> list = new ArrayList<>(); 
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = 0; j <arr[i].length ; j++) {
+                
+
+                }
+            }
+        return list;
+        }
 }
+    
