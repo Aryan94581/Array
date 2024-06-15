72fb6206ad7bee795827169e7d09f78a29b52e69
Aryan94581 <swamiaryan611@gmail.com>
2024-06-15 15:31:03 +0530

    update

--- L_Wealth2DArray.java
+++ L_Wealth2DArray.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 //richest-customer-wealth
 public class L_Wealth2DArray {
@@ -9,16 +10,19 @@ public class L_Wealth2DArray {
     
     
     }
-    static ArrayList<Integer> wealthCount(int[][] arr){
+    static int maximumWealth(int[][] arr){
+        
         ArrayList<Integer> list = new ArrayList<>(); 
         for (int i = 0; i < arr.length; i++) {
             int temp = 0;
             for (int j = 0; j <arr[i].length ; j++) {
-                
-
+                temp += arr[i][j];
                 }
+                list.add(temp);
             }
-        return list;
+            Collections.sort(list);
+            int max = list.getLast();
+        return max;
         }
 }
     
